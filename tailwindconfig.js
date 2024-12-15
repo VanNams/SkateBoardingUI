03873be82f1cb33c4d67f96b38962233aa13de59
VanNam <vannam16032003@gmail.com>
2024-12-16 03:57:34 +0700

    SkateBoard

---
+++ tailwindconfig.js
@@ -0,0 +1,10 @@
+tailwind.config = {
+    theme: {
+      extend: {
+        colors: {
+          primary: 'rgb(255 252 247)',
+          secondary: 'rgb(230 113 11)'
+        }
+      }
+    }
+  }
